7aad533d922fa23c476fce63b87a4a9522efd1fc
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-12-27 15:56:33 +0900

    [Update] Camera Moving 부드럽게 수정

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -4,39 +4,37 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 
-public class CamaraMovement : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
     public float edgeSize = 10f; // 가장자리 감지 범위q
-    public float moveSpeed = 15f; // 카메라 이동 속도
+    public float moveSpeed = 110f; // 카메라 이동 속도
     public Vector2 xBounds = new Vector2(-250, 250); // x축으로 이동하는 범위
     public Vector2 zBounds = new Vector2(-250, 250); // z축으로 이동하는 범위
 
     // 화면 모서리에 마우스의 위치 2차원 x,y 좌표를 게임상의 3차원 x,z에 적용시킴
     void Update()
     {
-        Vector3 pos = transform.position;
+        Vector3 pos = transform.position; // 물체 위치를 지정할 변수
 
         // 마우스 위치 가져오기
         Vector3 mousePosition = Input.mousePosition;
 
         // 화면 가장자리 체크
-        if (mousePosition.x < edgeSize && pos.x > xBounds.x) // 마우스가 화면 왼쪽
-        {
+        // 마우스가 화면 왼쪽
+        if (mousePosition.x < edgeSize && pos.x > xBounds.x)
             pos.x -= moveSpeed * Time.deltaTime;
-        }
-        else if (mousePosition.x > Screen.width - edgeSize && pos.x < xBounds.y) // 마우스가 화면 오른쪽
-        {
+        
+        // 마우스가 화면 오른쪽
+        else if (mousePosition.x > Screen.width - edgeSize && pos.x < xBounds.y)
             pos.x +=  moveSpeed * Time.deltaTime;
-        }
 
-        if (mousePosition.y < edgeSize && pos.z > zBounds.x) // 마우스가 화면 아래쪽
-        {
+        // 마우스가 화면 아래쪽
+        if (mousePosition.y < edgeSize && pos.z > zBounds.x)
             pos.z -= moveSpeed * Time.deltaTime;
-        }
-        else if (mousePosition.y > Screen.height - edgeSize && pos.z < zBounds.y) // 마우스가 화면 위쪽
-        {
+        
+        // 마우스가 화면 위쪽
+        else if (mousePosition.y > Screen.height - edgeSize && pos.z < zBounds.y)
             pos.z += moveSpeed * Time.deltaTime;
-        }
 
         // 카메라 위치 업데이트
         transform.position = pos;
