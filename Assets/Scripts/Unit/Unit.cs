10277707e734a5dd432883117e88f11aeaedca5e
정해윤 <jhy041020@gmail.com>
2024-12-30 14:58:11 +0900

    [Feat] Unit class 추가

---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit
+{
+    public string teamID {get; set;}
+    public int unitID {get; set;}
+    public int unitType {get; set;}
+    Vector3 unitLocation{get; set;}
+    public int unitHealth{get; set;}
+    public int unitCost{get; set;}
+    public int unitPower{get; set;}
+    public int unitPowerRange{get; set;}
+    public int unitMoveSpeed{get; set;}
+    public int populationCost{get; set;}
+    public Unit(string teamID,
+                int unitID,
+                int unitType,
+                Vector3 unitLocation, 
+                int unitHealth, 
+                int unitCost,
+                int unitPower,
+                int unitPowerRange,
+                int unitMoveSpeed,
+                int populationCost)
+                {
+                    this.teamID = teamID;
+                    this.unitID = unitID;
+                    this.unitType = unitType;
+                    this.unitLocation = unitLocation;
+                    this.unitHealth = unitHealth;
+                    this.unitCost = unitCost;
+                    this.unitPower = unitPower;
+                    this.unitPowerRange = unitPowerRange;
+                    this.unitMoveSpeed = unitMoveSpeed;
+                    this.populationCost = populationCost;
+                }
+}
