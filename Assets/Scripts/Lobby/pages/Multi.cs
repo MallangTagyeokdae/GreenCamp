48e6f82de285975232576c315bb77c1de72f0943
정해윤 <jhy041020@gmail.com>
2025-02-03 15:58:10 +0900

    [Feat] 팀 설정 페이지 수정 및 입력 필드 초기화 관련 버그 수정

--- Assets/Scripts/Lobby/pages/Multi.cs
+++ Assets/Scripts/Lobby/pages/Multi.cs
@@ -4,21 +4,25 @@ using Photon.Pun;
 using TMPro;
 using UnityEngine;
 
-public class Multi :LobbyState{
+public class Multi : LobbyState
+{
     private GameObject popUp;
-    private TMP_Text roomName;
+    private TMP_InputField roomName;
     private Transform parent;
 
-    public void InitPage(){ //하드코딩했음,,,
+    public void InitPage()
+    { //하드코딩했음,,,
         GameObject go = GameObject.Find("MultiModePage");
         parent = go.transform;
         popUp = parent.Find("CreateRoomArea").gameObject;
-        roomName = popUp.transform.Find("RoomNameField").Find("Text Area").Find("Text").GetComponent<TMP_Text>();
-        popUp.SetActive(false);
+        roomName = popUp.transform.Find("RoomNameField").GetComponent<TMP_InputField>();
         roomName.text = "";
+        popUp.SetActive(false);
     }
-    public void OutPage(string next){
-        if(next == "TeamSelect" && PhotonNetwork.NetworkClientState == Photon.Realtime.ClientState.JoinedLobby){
+    public void OutPage(string next)
+    {
+        if (next == "TeamSelect" && PhotonNetwork.NetworkClientState == Photon.Realtime.ClientState.JoinedLobby)
+        {
             PhotonManager.instance.CreateRoom(roomName.text);
         }
     }
--- Assets/Scripts/Lobby/pages/TeamSelect.cs
+++ Assets/Scripts/Lobby/pages/TeamSelect.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeamSelect : LobbyState{
+public class TeamSelect : LobbyState
+{
     private GameObject _uiGameObject = GameObject.Find("UI");
     private GameObject _teamSelectPage = GameObject.Find("SelectTeamUI");
-    public void InitPage(){
+    public void InitPage()
+    {
         _uiGameObject.SetActive(false);
         //Debug.Log(_teamSelectPage.name);
         //_teamSelectPage.SetActive(true);
     }
-    public void OutPage(string next){
+    public void OutPage(string next)
+    {
         _uiGameObject.SetActive(true);
-        _teamSelectPage.SetActive(false);
+        // _teamSelectPage.SetActive(false);
     }
 }
