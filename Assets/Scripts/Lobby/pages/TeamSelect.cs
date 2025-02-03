e4fc2dee35b05666a787186005e9ad3d64f3e8c5
정해윤 <jhy041020@gmail.com>
2025-02-03 19:22:10 +0900

    [Feat] 방장이면 시작 버튼 활성화

--- Assets/Scripts/Lobby/pages/TeamSelect.cs
+++ Assets/Scripts/Lobby/pages/TeamSelect.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Doozy.Runtime.UIManager.Components;
+using Photon.Pun;
 using UnityEngine;
 
 public class TeamSelect : LobbyState
 {
     private GameObject _uiGameObject = GameObject.Find("UI");
     private GameObject _teamSelectPage = GameObject.Find("SelectTeamUI");
+    private GameObject _gameStartBtn;
     public void InitPage()
     {
         _uiGameObject.SetActive(false);
+        _gameStartBtn = _teamSelectPage.transform.Find("GameStartBtn").gameObject;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            // Debug.Log("이새끼가실행돼야되는데");
+            _gameStartBtn.GetComponent<UIButton>().interactable = true;
+        }
+        else
+        {
+            // Debug.Log("왜이새끼가실행되지");
+            _gameStartBtn.GetComponent<UIButton>().interactable = false;
+        }
         //Debug.Log(_teamSelectPage.name);
         //_teamSelectPage.SetActive(true);
     }
