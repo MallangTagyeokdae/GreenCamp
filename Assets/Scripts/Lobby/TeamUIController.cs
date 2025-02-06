ad95da3251acb4d2c4b4d955497faad708543858
정해윤 <jhy041020@gmail.com>
2025-02-06 13:20:45 +0900

    [Feat] 팀 선택 버튼 -> 토그ㅜㄹ

--- Assets/Scripts/Lobby/TeamUIController.cs
+++ Assets/Scripts/Lobby/TeamUIController.cs
@@ -10,66 +10,79 @@ using UnityEngine.UIElements;
 
 public class TeamUIController : MonoBehaviour
 {
-    public GameObject BlueBtn;
+    public GameObject BlueToggle;
     public GameObject BluePlayer;
-    public GameObject RedBtn;
+    public GameObject RedToggle;
     public GameObject RedPlayer;
 
-    public void OnTeamSelect(Player player){
+    public void OnTeamSelect(Player player)
+    {
         string playerTeam = PhotonManager.instance.GetTeam(player);
         DeselectTeam(player);
-        if(playerTeam == "Blue"){
-            if(player != PhotonNetwork.LocalPlayer){
-                BlueBtn.GetComponent<UIButton>().interactable = false;
+        if (playerTeam == "Blue")
+        {
+            if (player != PhotonNetwork.LocalPlayer)
+            {
+                BlueToggle.GetComponent<UIToggle>().interactable = false;
             }
             BluePlayer.transform.parent.gameObject.SetActive(true);
             BluePlayer.GetComponent<TMP_Text>().text = player.NickName;
         }
 
-        else if(playerTeam == "Red"){
-            if(player != PhotonNetwork.LocalPlayer){
-                RedBtn.GetComponent<UIButton>().interactable = false;
+        else if (playerTeam == "Red")
+        {
+            if (player != PhotonNetwork.LocalPlayer)
+            {
+                RedToggle.GetComponent<UIToggle>().interactable = false;
             }
             RedPlayer.transform.parent.gameObject.SetActive(true);
             RedPlayer.GetComponent<TMP_Text>().text = player.NickName;
         }
 
-        else{
+        else
+        {
             Debug.Log("No team selected");
         }
     }
-    
 
-    public void DeselectTeam(Player player){
+
+    public void DeselectTeam(Player player)
+    {
         //팀 선택 이전에 팀이 있었으면 처리할 것들
         string playerTeam = PhotonManager.instance.GetPreviousTeam(player); //이전 팀으로 해야하는데,,?
-        if(playerTeam == "Blue"){
-            if(player != PhotonNetwork.LocalPlayer){
-                BlueBtn.GetComponent<UIButton>().interactable = true;
+        if (playerTeam == "Blue")
+        {
+            if (player != PhotonNetwork.LocalPlayer)
+            {
+                BlueToggle.GetComponent<UIToggle>().interactable = true;
             }
             BluePlayer.GetComponent<TMP_Text>().text = player.NickName;
             BluePlayer.transform.parent.gameObject.SetActive(false);
         }
 
-        else if(playerTeam == "Red"){
-            if(player != PhotonNetwork.LocalPlayer){
-                RedBtn.GetComponent<UIButton>().interactable = true;
+        else if (playerTeam == "Red")
+        {
+            if (player != PhotonNetwork.LocalPlayer)
+            {
+                RedToggle.GetComponent<UIToggle>().interactable = true;
             }
             RedPlayer.GetComponent<TMP_Text>().text = player.NickName;
             RedPlayer.transform.parent.gameObject.SetActive(false);
         }
 
-        else{
+        else
+        {
             Debug.Log("No team selected");
         }
     }
 
-    public void SendTeamSelect(){
+    public void SendTeamSelect()
+    {
         Debug.Log("send check");
         byte eventCode = 1;
         object player = PhotonNetwork.LocalPlayer; //팀을 변경한 플레이어 정보를 모든 클라이언트에게 전송
-        RaiseEventOptions options = new RaiseEventOptions {Receivers = ReceiverGroup.All}; //ALL, OTHERS, MASTER CLIENT 중에서 선택 (서버에 동기화되는 시점과 타이밍을 맞추기 위해서 ALL사용)
-        SendOptions sendOptions = new SendOptions{Reliability = true}; // TCP, UDP 통신 중에서 선택
+        RaiseEventOptions options = new RaiseEventOptions { Receivers = ReceiverGroup.All }; //ALL, OTHERS, MASTER CLIENT 중에서 선택 (서버에 동기화되는 시점과 타이밍을 맞추기 위해서 ALL사용)
+        SendOptions sendOptions = new SendOptions { Reliability = true }; // TCP, UDP 통신 중에서 선택
         PhotonNetwork.RaiseEvent(eventCode, player, options, sendOptions);
     }
 }
