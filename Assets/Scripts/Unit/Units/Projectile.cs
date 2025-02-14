0ddc1fc23ccc4fc7266cde174d551f5d787e3908
YoungRheeCho <nrcho0331@gmail.com>
2025-02-14 23:32:35 +0900

    [ifx] 발사체 한 발에 여러 번 피 깎이는 버그 수정
    [ifx] 발사체 한 발에 여러 번 피 깎이는 버그 수정

--- Assets/Scripts/Unit/Units/Projectile.cs
+++ Assets/Scripts/Unit/Units/Projectile.cs
@@ -8,29 +8,23 @@ public class Projectile : MonoBehaviour
     private GameObject _target;
     private Unit _unit;
     private Coroutine coroutine;
-    //private UnityEngine.Vector3 initPos;
-
 
     private void Awake(){
-        //initPos = transform.localPosition;
     }
 
-    private void OnDisable() {
-        //수정해야함
-        /*if(_unit.unitType == "Archer"){
-            transform.position = transform.parent.TransformPoint(initPos);
-        }*/
-    }
 
     private void OnTriggerEnter(Collider other) {
+        Debug.Log($"발사체가 맞춘 collider: {other.name}, taget인가? {other.gameObject == _target}");
         if(other.gameObject == _target){
-            if(gameObject.transform.parent.gameObject.GetComponent<PhotonView>().IsMine == false){  //화살을 쏜 유닛이 자신의 유닛이 아닌 경우 return
+            Debug.Log("이건 제대로 되나?");
+            if(_unit.gameObject.transform.parent.gameObject.GetComponent<PhotonView>().IsMine == false){  //화살을 쏜 유닛이 자신의 유닛이 아닌 경우 return
+                Debug.Log("check trigger");
                 StopCoroutine(coroutine);
-                //gameObject.SetActive(false);
                 Destroy(gameObject);
                 return;
             }
             else{
+                Debug.Log("이건 제대로 되나?2");
                 if(_target != null){
                     _target.GetComponent<PhotonView>().RPC("AttackRequest", RpcTarget.MasterClient, _unit.unitPower);  //화살을 쏜 유닛이 자신의 유닛인 경우 적에게 맞았을 때 마스터 클라이언트에게 판정을 요구
                 }
@@ -51,19 +45,23 @@ public class Projectile : MonoBehaviour
     private IEnumerator Launch(){
         UnityEngine.Vector3 currentPos = UnityEngine.Vector3.zero;;
         UnityEngine.Vector3 targetPos;
-        for(float time = 0f; time < 1f; time += Time.deltaTime){
+        for(float time = 0f; time < 2f; time += Time.deltaTime){
             if(_target != null){
                 currentPos = gameObject.transform.position;
                 targetPos = _target.transform.position + new UnityEngine.Vector3(0, 1.2f, 0);
                 float distance = UnityEngine.Vector3.Distance(currentPos, targetPos);
                 UnityEngine.Vector3 dir = (targetPos - currentPos).normalized;
-                transform.position += dir * distance * time/1f;
+                if(dir != Vector3.zero){
+                    transform.rotation = Quaternion.LookRotation(dir) * Quaternion.Euler(-90, 0, 0);
+                }
+                transform.position += dir * distance * time/2f;
 
                 yield return null;
             }
         }
 
-        if(_target != null){
+        if(_target != null && _unit.gameObject.transform.parent.gameObject.GetComponent<PhotonView>().IsMine){
+            Debug.Log("check Launch");
             _target.GetComponent<PhotonView>().RPC("AttackRequest", RpcTarget.MasterClient, _unit.unitPower);
         }
         Destroy(gameObject);
