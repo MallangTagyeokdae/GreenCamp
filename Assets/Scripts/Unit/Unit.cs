533eb0fd1c5994fb4ec96c8a89f059b968cb8758
정해윤 <jhy041020@gmail.com>
2025-02-20 17:49:14 +0900

    Update Unit.cs

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -250,7 +250,7 @@ public abstract class Unit : Entity
                     }
                     else
                     {
-
+                        animator.SetBool("isAttacking", true);
                     }
                 }
                 break;
