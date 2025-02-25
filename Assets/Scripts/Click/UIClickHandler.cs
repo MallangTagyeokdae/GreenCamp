6fb86491ce8dffeaf3a28a8c0386cc9027686b82
YoungRheeCho <nrcho0331@gmail.com>
2025-02-25 17:15:44 +0900

    [update] 미니맵에 클릭 구현 ver2
    우클 좌클 구분, 미니맵 뒤에 있는 객체 클릭 막음

--- Assets/Scripts/Click/UIClickHandler.cs
+++ Assets/Scripts/Click/UIClickHandler.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UIClickHandler : MonoBehaviour, IPointerClickHandler
+public class UIClickHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
     public GameObject target;
+    public GameObject clickManger;
     public void OnPointerClick(PointerEventData eventData)
     {
-        target.transform.position = CalculateAxis(eventData.position.x, eventData.position.y);
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            target.transform.position = CalculateAxis(eventData.position.x, eventData.position.y);
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            //해당 위치에 그라운드를 클릭하는 판정으로 만들면 될듯? 해당 그라운드의 event를 invoke하는 방식으로
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        clickManger.SetActive(false);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        clickManger.SetActive(true);
     }
 
     Vector3 CalculateAxis(float x, float y){
